4bedd3bdb8c43574e0065b8c1151991420400029
Cleyton Mendes <cleyton.mendest@gmail.com>
2023-04-29 13:35:19 -0300

    inclui login/forgot/expired

---
+++ src/app/(login)/login/forgot/expired/page.tsx
@@ -0,0 +1,26 @@
+"use client"
+
+import { Alert, Link as MuiLink } from '@mui/material'
+import Link from 'next/link'
+import React from 'react'
+
+const Page = () => {
+    return (
+        <>
+            <Alert
+                variant='filled'
+                severity='error'
+                sx={{ mt: 3, mb: 3 }}>
+                    Este link expirou, refaça o procedimento...
+            </Alert>
+            <MuiLink
+                href='/login/forgot'
+                component={Link}
+                variant='button'>
+                    Esqueci minha senha
+                </MuiLink>
+        </>
+    )
+}
+
+export default Page
